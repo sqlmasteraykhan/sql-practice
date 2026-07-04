8c243baeac739bac8231cfcebeb6bc38ca644426
sqlmasteraykhan <ayxanqurbanov341@gmail.com>
2026-07-04 19:02:49 +0400

    Create between.sql

---
+++ between.sql
@@ -0,0 +1,11 @@
+-- BETWEEN Examples
+
+-- Customers between ages 20 and 30
+SELECT *
+FROM customers
+WHERE age BETWEEN 20 AND 30;
+
+-- Salaries between 3000 and 6000
+SELECT *
+FROM customers
+WHERE salary BETWEEN 3000 AND 6000;
