ea8c4f7dc676f955c3b1408a24cd07587d2ea57a
sqlmasteraykhan <ayxanqurbanov341@gmail.com>
2026-07-04 19:02:05 +0400

    Create distinct.sql

---
+++ distinct.sql
@@ -0,0 +1,13 @@
+-- DISTINCT Examples
+
+-- Show unique cities
+SELECT DISTINCT city
+FROM customers;
+
+-- Show unique customer names
+SELECT DISTINCT customer_name
+FROM customers;
+
+-- Show unique ages
+SELECT DISTINCT age
+FROM customers;
